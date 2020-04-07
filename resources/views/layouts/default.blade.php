6521cb29dbdf6c2805f164cca270398174cf1613
momardevwm <63321336+momardevwm@users.noreply.github.com>
2020-04-08 01:33:18 +0200

    Add page_title

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laracarte</title>
+    <title>{{isset($title) ? $title . '|' : ''}} Laracarte-List of artisans</title>
     <style media="screen">
       footer{
         margin:4em 0;
